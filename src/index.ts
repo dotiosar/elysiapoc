b3490892bca0522613df5c86a0d8b9b5ea297f37
dotiosar <sarahbukansechan@proton.me>
2024-08-17 12:25:33 +0700

    add html meta title

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { Elysia } from "elysia";
+import { readFileSync } from "fs";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+
+app.get("/", () => {
+  const html = readFileSync("public/index.html", "utf-8");
+  return new Response(html, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
+});
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
