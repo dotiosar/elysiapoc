e572e69853c045282f05f8e0a42618d05178ecaf
dotiosar <sarahbukansechan@proton.me>
2024-08-17 13:53:02 +0700

    add prisma

---
+++ prisma/migrations/20240817064005_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "emailVerified" TEXT NOT NULL DEFAULT 'not verified',
+    "role" TEXT NOT NULL DEFAULT 'merchant',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
